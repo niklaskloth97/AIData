d12ced6ff121c2a7f5dbaaba2e04a3b5357764a8
Lukas Monert <lukas.monert@kdo.de>
2024-12-30 17:21:57 +0100

    Add TanStack Query

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,29 +1,34 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const metadata: Metadata = {
-  title: "Hilti Project Seminar",
-  description: "Frontend for Hilti Project Seminar",
+    title: "Hilti Project Seminar",
+    description: "Frontend for Hilti Project Seminar",
 };
 
+const queryClient = new QueryClient();
+
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body>
+                <QueryClientProvider client={queryClient}>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                    >
+                        {children}
+                    </ThemeProvider>
+                </QueryClientProvider>
+            </body>
+        </html>
+    );
 }
