816dc7b13de496222c7d2d2594dfa33b1d825b58
mzikria <mzikria@uni-muenster.de>
2025-01-03 20:09:56 +0100

    user-manual: add page.tsx

---
+++ frontend/app/user-manual/page.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+const UserManual = () => {
+    return (
+        <div style={{ fontFamily: 'Arial, sans-serif', display: 'flex', maxWidth: '1500px', marginLeft: '200px', marginTop: '20px', padding: '20px', lineHeight: '1.3' }}>
+            {/* Main Content */}
+            <div style={{ flex: '3', paddingRight: '20px' }}>
+                <header style={{ borderBottom: '2px solid #ddd', paddingBottom: '10px', marginBottom: '20px' }}>
+                    <h1 style={{ fontSize: '2rem', margin: 0 }}>User Manual</h1>
+                </header>
+
+                <main>
+                    <section style={{ marginBottom: '30px' }}>
+                        <h2 style={{ fontSize: '1.5rem', marginBottom: '10px' }}>Getting started</h2>
+                        <p style={{ fontSize: '0.875rem' }}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                        </p>
+                    </section>
+
+                    <section style={{ marginBottom: '30px' }}>
+                        <h2 style={{ fontSize: '1.5rem', marginBottom: '10px' }}>Moving along</h2>
+                        <p style={{ fontSize: '0.875rem',  marginBottom: '10px' }}>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+                            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.   </p>
+
+                            <p style={{ fontSize: '0.875rem',  marginBottom: '10px' }}>
+                            Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+
+                            Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+
+                            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.
+                        </p>
+
+
+                        <p style={{ fontSize: '0.875rem', marginBottom: '10px' }}>  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+                            Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+
+                            Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+
+                            Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper s
+                        </p>
+                    </section>
+
+                    <section style={{ marginBottom: '30px' }}>
+                        <h2 style={{ fontSize: '1.5rem', marginBottom: '10px' }}>Connect the first database</h2>
+                        <p style={{ fontSize: '0.875rem' }}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                        </p>
+                    </section>
+                </main>
+            </div>
+
+            {/* Table of Contents */}
+            <aside style={{ flex: '0.7', paddingLeft: '5px' }}>
+                <div style={{ padding: '10px', border: '1px solid #ddd', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
+                    <h2 style={{ fontSize: '1rem', marginBottom: '10px', fontWeight: 'bold' }}>User Manual</h2>
+                    <ul style={{ listStyle: 'none', padding: 0, margin: 0, fontSize: '0.9rem' }}>
+                        <li style={{ marginBottom: '1px' }}>Getting started</li>
+                        <li style={{ marginBottom: '1px' }}>Moving along</li>
+                        <li style={{ marginBottom: '1px' }}>Connect the first database</li>
+                        <li style={{ marginBottom: '1px' }}>User Interface Overview</li>
+                        <li style={{ marginBottom: '1px' }}>Core Features and Functionality</li>
+                        <li style={{ marginBottom: '1px' }}>Customization</li>
+                        <li style={{ marginBottom: '1px' }}>Troubleshooting and FAQs</li>
+                    </ul>
+                </div>
+            </aside>
+        </div>
+    );
+};
+export default UserManual;
