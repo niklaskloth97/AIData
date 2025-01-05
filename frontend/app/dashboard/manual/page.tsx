329afd3ac35016c84589db2d6e561f9f345b0dd3
Lukas Monert <lukas.monert@kdo.de>
2025-01-05 17:40:52 +0100

    Adding markdown renderring via mdx

--- frontend/app/dashboard/manual/page.tsx
+++ frontend/app/dashboard/manual/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
 import PageHeader from "@/components/PageHeader";
+import Manual from "@/markdown/manual.mdx";
 
 export default function Page() {
     return (
         <>
             <PageHeader heading="Manual" subtext="This is the subtext."/>
+            <Manual />
         </>
     );
 }
