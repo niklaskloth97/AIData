b473ce99e4d282f312662c6a0670517fad705ee9
TpnHTW <philipnartschik96@gmail.com>
2025-01-07 19:14:46 +0100

    Update docker compose to start restapi aswell.

--- backend/langgraph_frontend/restapi/models.py
+++ backend/langgraph_frontend/restapi/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
