aa3a67ee6740817aaff85bd47cbd86cebd05ba46
mzikria <mzikria@uni-muenster.de>
2025-01-08 22:28:15 +0100

    // Contents: // // Renamed frontend/app/welcome-user/layout.tsx -> frontend/app/start-page/layout.tsx // Renamed frontend/app/welcome-user/page.tsx -> frontend/app/start-page/page.tsx // Added frontend/public/DataProcessingFlowchart.webp

--- frontend/app/start-page/page.tsx
+++ frontend/app/start-page/page.tsx
@@ -3,9 +3,9 @@ export default function Home() {
       <div style={{ padding: '10px'}}>
         <h1 className="text-2xl font-bold mb-4">Welcome User!</h1>
         <p className="mb-6">Here is a diagram of how this all works:</p>
-        <div style={{ paddingLeft: '100px', paddingTop: '85px' }}>
+        <div style={{ paddingLeft: '50px', paddingRight: '50px', paddingTop: '85px' }}>
         <img
-          src="/welcome-user.png"
+          src="/DataProcessingFlowchart.webp"
           alt="Data Processing Flowchart"
           className="max-w-full h-auto"
         />
