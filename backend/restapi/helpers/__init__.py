fcaa4ce0980ed88ad8aabb2504861ccf50afd8e6
TpnHTW <philipnartschik96@gmail.com>
2025-01-12 19:42:01 +0100

    populatedb update

--- backend/restapi/helpers/__init__.py
+++ backend/restapi/helpers/__init__.py
@@ -2,5 +2,7 @@ from .populate_projecttables import populate_projecttables
 from .populate_caseIDs import populate_caseids
 from .populate_mappings import populate_mappings
 from .populate_process import populate_process
+from .populate_possible_mappings import populate_possible_mappings
+from .populate_additional_events import populate_additional_events
 
-__all__ = ["populate_projecttables", "populate_caseids", "populate_mappings", "populate_process"]
+__all__ = ["populate_projecttables", "populate_caseids", "populate_mappings", "populate_process", "populate_possible_mappings", "populate_additional_events"]
