c9b271d1b7c702ef175555635d048109013eced3
TpnHTW <philipnartschik96@gmail.com>
2025-01-13 13:47:56 +0100

    change mapping object

--- backend/restapi/models/Mapping.py
+++ backend/restapi/models/Mapping.py
@@ -13,6 +13,7 @@ class Mapping(Base):
     timestampColumn = Column(String, nullable=False)  # Format: Table.Column
     eventType = Column(String, nullable=False)
     otherAttributes = Column(JSON)  # List of Table.Column strings
+    tableInvolved = Column(String, nullable=False)
 
 
 class MappingSchema(BaseModel):
@@ -21,10 +22,10 @@ class MappingSchema(BaseModel):
     timestampColumn: str
     eventType: str
     otherAttributes: List[str] = Field(default_factory=list)
+    tableInvolved: str
 
 class MappingsResponseSchema(BaseModel):
     mappings: List[MappingSchema]
-    options: dict
 
     class Config:
         from_attributes = True
@@ -34,3 +35,4 @@ class CreateMappingSchema(BaseModel):
     timestamp_column: str
     eventtype: str
     otherAttributes: List[str]
+    tableInvolved: str
